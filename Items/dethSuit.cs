67b52731270add80d4cf009803c41e5ffd4143b5
dethMastery <owner@detzz.in.th>
2022-05-29 12:07:53 +0700

    updated: tileid again

--- Items/dethSuit.cs
+++ Items/dethSuit.cs
@@ -25,7 +25,7 @@ namespace dethzITEMz.Items.Armor
         {
             ModRecipe recipe = new ModRecipe(mod);
             recipe.AddIngredient(ItemID.DirtBlock, 1);
-             recipe.AddTile(TileID.Anvils);
+             recipe.AddTile(16);
             recipe.SetResult(this);
             recipe.AddRecipe();
         }
